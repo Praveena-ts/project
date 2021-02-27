270504a0eb6ac8b1711df8313c869d5b285d1bd7
janmejlk <77544086+janmejlk@users.noreply.github.com>
2021-02-26 23:12:12 -0500

    Lalit_ListChanges

---
+++ FinalProjectGroup11/FinalProjectGroup11/Pages/Agents/List.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectGroup11.Data;
+using FinalProjectGroup11.Models;
+
+namespace FinalProjectGroup11.Pages.Agents
+{
+    public class ListModel : PageModel
+    {
+        private readonly FinalProjectGroup11.Data.FinalProjectGroup11Context _context;
+
+        public ListModel(FinalProjectGroup11.Data.FinalProjectGroup11Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Agent> Agent { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Agent = await _context.Agent
+                .Include(a => a.SalesOffice).ToListAsync();
+        }
+    }
+}
---
+++ FinalProjectGroup11/FinalProjectGroup11/Pages/SalesOffices/List.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectGroup11.Data;
+using FinalProjectGroup11.Models;
+
+namespace FinalProjectGroup11.Pages.SalesOffices
+{
+    public class ListModel : PageModel
+    {
+        private readonly FinalProjectGroup11.Data.FinalProjectGroup11Context _context;
+
+        public ListModel(FinalProjectGroup11.Data.FinalProjectGroup11Context context)
+        {
+            _context = context;
+        }
+
+        public IList<SalesOffice> SalesOffice { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            SalesOffice = await _context.SalesOffice.ToListAsync();
+        }
+    }
+}
