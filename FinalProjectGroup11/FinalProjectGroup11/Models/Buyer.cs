2c945b33af22248fa38e18c2df3156cfeae9b38b
janmejlk <77544086+janmejlk@users.noreply.github.com>
2021-02-26 23:28:53 -0500

    changes123

--- FinalProjectGroup11/FinalProjectGroup11/Models/Buyer.cs
+++ FinalProjectGroup11/FinalProjectGroup11/Models/Buyer.cs
@@ -10,19 +10,24 @@ namespace FinalProjectGroup11.Models
     public class Buyer
     {
         public int Id { get; set; }
-        
-        public string FirstName { get; set; }
+
         [DisplayName("First Name")]
         [Required(ErrorMessage = "First Name is Required")]
         [StringLength(30)]
-        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        
         [DisplayName("Last Name")]
         [Required(ErrorMessage = "Last Name is Required")]
         [StringLength(30)]
-        public string PhoneNumber { get; set; }
+        public string LastName { get; set; }
+        
         [DisplayName("Phone Number")]
         [Required(ErrorMessage = "Phone Number is Required")]
         [StringLength(10)]
+        public string PhoneNumber { get; set; }
+       
+        
+        
         public decimal Budget { get; set; }
         public List<Dwelling> Dwellings { get; set; }
     }
